95adf384895b21fb12282b09732e01725767275b
FarazBagwan <farazbagwan11@gmail.com>
2022-09-26 14:06:05 +0530

    Commit-1

---
+++ tf-example/main.tf
@@ -0,0 +1,40 @@
+data "aws_ami" "windows" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["Windows_Server-2012-R2_RTM-English-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+    
+    owners = ["801119661308"] # Canonical
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+data "aws_iam_role" "myrole" {
+  name = "myfullaccessrole"
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "myprofile"
+  role = data.aws_iam_role.myrole.name
+}
+
+resource "aws_instance" "win_server" {
+  ami           = data.aws_ami.windows.id
+  instance_type = "t2.micro"
+  key_name      = "MyEC2Instance"
+  security_groups = [ "mySG" ]
+  iam_instance_profile = aws_iam_instance_profile.test_profile.name
+
+  tags = {
+    Name = var.ec2_name
+  }
+}
